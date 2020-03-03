c20a01d5a4cb7475930fca0ad42551ea4687c2be
Guifp1109 <61754273+Guifp1109@users.noreply.github.com>
2020-03-03 15:15:03 -0300

    Teste De Envio

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("Guilherme Francisco Passos\n");
+printf("Guilhermefp1109@gmail.com\n");
+getchar();//captura um caractere do teclado
+system("Clear");//Limpa a tela
+printf("Fernando Barreto\n");
+printf("fbarreto@utfpr.edu.br\n");
+return 0;
+}
